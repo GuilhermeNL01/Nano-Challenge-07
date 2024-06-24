619835eaa6d7fd0029a5b1d38b3a5e08e0a4d43e
RoxasTA <victorbr64g@gmail.com>
2024-06-24 17:44:42 -0300

    Revert "Novo método"
    This reverts commit 1b2adab1efddc934680055b2a18e335e0800eaf6.

--- NanoChallenge07/NanoChallenge07/Utils/NetworkManager.swift
+++ NanoChallenge07/NanoChallenge07/Utils/NetworkManager.swift
@@ -9,46 +9,98 @@ import Foundation
 
 class NetworkManager {
     
-    private func fetchData<T: Decodable>(from endpoint: String, responseType: T.Type) async throws -> T {
+    func fetchItems(item:String) async throws -> Result{
+        var result = Result(Results: [])
+        let endpoint = "https://xivapi.com/search?string=\(item)"
         guard let url = URL(string: endpoint) else { throw GHError.invalidURl }
-        let (data, response) = try await URLSession.shared.data(from: url)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+        let (data,response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            print("\(response)")
             throw GHError.invalidREsponse
         }
-        
-        do {
+        do{
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let decodedData = try decoder.decode(T.self, from: data)
-            return decodedData
+            result = try decoder.decode(Result.self, from: data)
+            return result
+        } catch {
+            print("result")
+            throw GHError.invalidData
+        }
+    }
+    
+    func fetchItemInfo(itemId: Int) async throws -> ItemInfo{
+        let endpoint = "https://xivapi.com/item/\(itemId)"
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURl }
+        let (data,response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            print("\(response)")
+            throw GHError.invalidREsponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let data = try decoder.decode(ItemInfo.self, from: data)
+            return data
         } catch {
             throw GHError.invalidData
         }
     }
     
-    func fetchItems(item: String) async throws -> Result {
-        let endpoint = "https://xivapi.com/search?string=\(item)"
-        return try await fetchData(from: endpoint, responseType: Result.self)
-    }
-    
-    func fetchItemInfo(itemId: Int) async throws -> ItemInfo {
-        let endpoint = "https://xivapi.com/item/\(itemId)"
-        return try await fetchData(from: endpoint, responseType: ItemInfo.self)
-    }
-    
-    func fetchAvailableDataCenters() async throws -> [DataCenter] {
+    func fetchAvailableDataCenters() async throws -> [DataCenter]{
+        var result:[DataCenter] = []
         let endpoint = "https://universalis.app/api/v2/data-centers"
-        return try await fetchData(from: endpoint, responseType: [DataCenter].self)
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURl }
+        let (data,response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            print("\(response)")
+            throw GHError.invalidREsponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            result = try decoder.decode([DataCenter].self, from: data)
+            return result
+        } catch {
+            throw GHError.invalidData
+        }
     }
     
-    func fetchAvailableWorlds() async throws -> [World] {
+    func fetchAvailableWorlds() async throws -> [World]{
+        var result:[World] = []
         let endpoint = "https://universalis.app/api/v2/worlds"
-        return try await fetchData(from: endpoint, responseType: [World].self)
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURl }
+        let (data,response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            print("\(response)")
+            throw GHError.invalidREsponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            result = try decoder.decode([World].self, from: data)
+            return result
+        } catch {
+            throw GHError.invalidData
+        }
     }
     
-    func searchPrices(world: Int, itemId: Int) async throws -> MarketInfo {
+    func searchPrices(world:Int, itemId:Int) async throws -> MarketInfo{
+        var result:MarketInfo = MarketInfo(worldID: world, minPrice: 0)
         let endpoint = "https://universalis.app/api/v2/\(world)/\(itemId)"
-        return try await fetchData(from: endpoint, responseType: MarketInfo.self)
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURl }
+        let (data,response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            print("\(response)")
+            throw GHError.invalidREsponse
+        }
+        do{
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            result = try decoder.decode(MarketInfo.self, from: data)
+            return result
+        } catch {
+            throw GHError.invalidData
+        }
     }
 }
