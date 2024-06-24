80094e7b4211599377b72db268b3e8c273004b13
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2024-06-24 15:08:55 -0300

    Projeto Xcode
    Criação do projeto no Xcode

---
+++ NanoChallenge07/NanoChallenge07/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NanoChallenge07
+//
+//  Created by Guilherme Nunes Lobo on 24/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NanoChallenge07/NanoChallenge07/NanoChallenge07App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge07App.swift
+//  NanoChallenge07
+//
+//  Created by Guilherme Nunes Lobo on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge07App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
