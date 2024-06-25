fe3ab4db0b8447881f6180cecad5536048d05637
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-06-25 15:09:17 -0300

    test: networkingmanager json decode failure

--- NanoChallenge07/NanoChallenge07UnitTests/Networking/NetworkingManagerTests.swift
+++ NanoChallenge07/NanoChallenge07UnitTests/Networking/NetworkingManagerTests.swift
@@ -54,7 +54,7 @@ final class NetworkingManagerTests: XCTestCase {
         }
     }
     
-    func testNetworkingManagerRequestFailure() async throws {
+    func testNetworkingManagerRequestServerFailure() async throws {
         MockURLSessionProtocol.handler = {
             let response = HTTPURLResponse(url: Endpoint.datacenters.url!, statusCode: 500, httpVersion: nil, headerFields: nil)!
             return (response, nil)
@@ -66,6 +66,36 @@ final class NetworkingManagerTests: XCTestCase {
         } catch {
             XCTAssertNotNil(error as? URLError)
             XCTAssertEqual(error as! URLError, URLError(.badServerResponse))
+        }
+    }
+    
+    func testNetworkingManagerRequestParseFailure() async throws {
+        MockURLSessionProtocol.handler = {
+            let response = HTTPURLResponse(url: Endpoint.datacenters.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+            let data = """
+                [
+                    {
+                        "name": "Elemental",
+                        "region": "Japan",
+                        "worlds": [
+                            45,
+                            49,
+                            50,
+                            58,
+                            68,
+                            72,
+                            90,
+                            94
+                        ]
+                    }
+                """.data(using: .utf8)!
+            return (response, data)
+        }
+        do {
+            let result = try await NetworkingManager.shared.request(session: session, .datacenters, type: [DataCenter].self)
+            XCTFail()
+        } catch {
+            XCTAssertNotNil(error as? DecodingError)
         }
     }
     
