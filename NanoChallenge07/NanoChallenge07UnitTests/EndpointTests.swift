2e5dfccdee7330fd3cf45d578d88272692f9157b
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-06-25 14:03:26 -0300

    test: endpoint path and host

--- NanoChallenge07/NanoChallenge07UnitTests/EndpointTests.swift
+++ NanoChallenge07/NanoChallenge07UnitTests/EndpointTests.swift
@@ -41,4 +41,50 @@ final class EndpointTests: XCTestCase {
         XCTAssertGreaterThan(sut.queryItems!.count, 0)
         XCTAssertTrue(sut.queryItems![0].name.contains("string"))
     }
+    
+    func testItemInfoEndpointHost() {
+        sut = .itemInfo(69)
+        XCTAssertFalse(sut.host.isEmpty)
+    }
+    
+    func testItemInfoEndpointPath() {
+        sut = .itemInfo(69)
+        XCTAssertFalse(sut.path.isEmpty)
+        XCTAssertTrue(sut.path.contains("69"))
+        XCTAssertTrue(sut.path.contains("item"))
+    }
+    
+    func testDatacentersEndpointHost() {
+        sut = .datacenters
+        XCTAssertFalse(sut.host.isEmpty)
+    }
+    
+    func testDatacentersEndpointPath() {
+        sut = .datacenters
+        XCTAssertFalse(sut.path.isEmpty)
+        XCTAssertTrue(sut.path.contains("data-centers"))
+    }
+    
+    func testWorldsEndpointHost() {
+        sut = .worlds
+        XCTAssertFalse(sut.host.isEmpty)
+    }
+    
+    func testWorldsEndpointPath() {
+        sut = .worlds
+        XCTAssertFalse(sut.path.isEmpty)
+        XCTAssertTrue(sut.path.contains("worlds"))
+    }
+    
+    func testItemPriceForWorldEndpointHost() {
+        sut = .itemPriceForWorld(12, 34)
+        XCTAssertFalse(sut.host.isEmpty)
+    }
+    
+    func testItemPriceForWorldEndpointPath() {
+        sut = .itemPriceForWorld(12, 34)
+        XCTAssertFalse(sut.path.isEmpty)
+        XCTAssertTrue(sut.path.contains("12"))
+        XCTAssertTrue(sut.path.contains("12"))
+    }
 }
