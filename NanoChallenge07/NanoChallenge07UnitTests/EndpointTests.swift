2f1876d9603c50e5be5c5f0dd05da766914ebb48
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-06-25 13:52:59 -0300

    test: add search endpoint tests

--- NanoChallenge07/NanoChallenge07UnitTests/EndpointTests.swift
+++ NanoChallenge07/NanoChallenge07UnitTests/EndpointTests.swift
@@ -9,10 +9,36 @@ import XCTest
 @testable import NanoChallenge07
 
 final class EndpointTests: XCTestCase {
+    var sut: Endpoint!
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
     func testEndpointsGenerateValidURL() {
-        var endpoints: [Endpoint] = [.searchItem("item"), .datacenters, .worlds,.itemInfo(49),.itemPriceForWorld(49, 420)]
+        let endpoints: [Endpoint] = [.searchItem("item"), .datacenters, .worlds,.itemInfo(49),.itemPriceForWorld(49, 420)]
         for endpoint in endpoints {
             XCTAssertNotNil(endpoint.url)
         }
+    }
+    
+    func testSearchEndpointPath() {
+        sut = .searchItem("item")
+        XCTAssertFalse(sut.path.isEmpty)
+        XCTAssertTrue(sut.path.contains("/"))
+        XCTAssertTrue(sut.path.contains("search"))
+    }
+    
+    func testSearchEndpointHost() {
+        sut = .searchItem("item")
+        XCTAssertFalse(sut.host.isEmpty)
+    }
+    
+    func testSearchEndpointQueryItems() {
+        sut = .searchItem("item")
+        XCTAssertNotNil(sut.queryItems)
+        XCTAssertFalse(sut.queryItems!.isEmpty)
+        XCTAssertGreaterThan(sut.queryItems!.count, 0)
+        XCTAssertTrue(sut.queryItems![0].name.contains("string"))
     }
 }
