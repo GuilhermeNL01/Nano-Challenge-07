8214a3c1779ab73daefc4caeb2e577a8e9e07c2c
RoxasTA <victorbr64g@gmail.com>
2024-06-26 15:50:47 -0300

    Testes de ui
    Teste de navegação e pesquisa
    Teste de ver informações

--- NanoChallenge07/NanoChallenge07UITests/NanoChallenge07UITests.swift
+++ NanoChallenge07/NanoChallenge07UITests/NanoChallenge07UITests.swift
@@ -9,20 +9,48 @@ import XCTest
 
 final class NanoChallenge07UITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    
+    @MainActor func testSearch() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        let pesquisaTextField = app.textFields["Pesquisa"]
+        //pesquisaTextField.tap()
+        //app.images["Search"].tap()
+        //app.scrollViews.otherElements.buttons["Dated Copper Scepter"].tap()
+        //app/*@START_MENU_TOKEN@*/.staticTexts["Elemental"]/*[[".buttons[\"Please choose a data center, Elemental\"].staticTexts[\"Elemental\"]",".staticTexts[\"Elemental\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        pesquisaTextField.tap()
+        pesquisaTextField.typeText("Copper")
+        
+        XCTAssertFalse(app.scrollViews.otherElements.buttons["Dated Copper Scepter"].exists)
+                
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    @MainActor func testSeeInfo() {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        let pesquisaTextField = app.textFields["Pesquisa"]
+        let elementsQuery = app.scrollViews.otherElements
+        //app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"].buttons["Search"].tap()
+        
+        pesquisaTextField.tap()
+        pesquisaTextField.typeText("Copper")
+        elementsQuery.buttons["Dated Copper Scepter"].tap()
+        elementsQuery.staticTexts["untradable"].tap()
+        XCTAssertFalse(elementsQuery.buttons["Dated Copper Scepter"].exists)
     }
-
-    func testExample() throws {
+    
+    @MainActor func testServerSelaction() {
+        
+        
+        
+    }
+    
+    @MainActor func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
