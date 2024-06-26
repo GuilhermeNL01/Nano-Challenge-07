ad13da7f44a00927c9c9d4c970c08e2a77ae4b23
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-06-26 14:30:00 -0300

    test: searchviewmodel

--- NanoChallenge07/NanoChallenge07UnitTests/ViewModels/SearchViewModelTest.swift
+++ NanoChallenge07/NanoChallenge07UnitTests/ViewModels/SearchViewModelTest.swift
@@ -5,4 +5,26 @@
 //  Created by Fabio Freitas on 26/06/24.
 //
 
-import Foundation
+import XCTest
+@testable import NanoChallenge07
+
+class SearchViewModelTest: XCTestCase {
+    var sut: SearchPageViewModel!
+    
+    @MainActor override func setUpWithError() throws {
+        sut = SearchPageViewModel(network: NetworkingManager.mock)
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    @MainActor func testSearchFunctionality() async throws {
+        sut.textToSearch =  "search"
+        XCTAssertFalse(sut.isSearching)
+        await sut.performSearch()
+        XCTAssertFalse(sut.result.Results.isEmpty)
+        XCTAssertFalse(sut.isSearching)
+    }
+}
+
